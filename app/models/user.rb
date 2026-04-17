22bce30be62aba5fba821fc29881e2bb69587f47
AbdallahEhab <abdallahehab3710@gmail.com>
2026-04-17 15:08:50 +0200

    feat: add validations for user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validate :name, presence: true
+  validate :dob, presence: true
+  validate :phone, presence: true
+  validate :email, presence: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP,
+            message: "must be a valid email address" }
 end
