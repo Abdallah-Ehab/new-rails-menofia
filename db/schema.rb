3c0cbb1dfb2dd25ed4fa5e4db2c837bda018cb6b
AbdallahEhab <abdallahehab3710@gmail.com>
2026-04-18 13:52:39 +0200

    Add Editor model, migration, fixtures, and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_18_114810) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_18_115210) do
+  create_table "editors", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
