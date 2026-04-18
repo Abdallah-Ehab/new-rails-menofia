35c1ccbc9ea7f0e994233d461fdf99c4d7dcbfd4
AbdallahEhab <abdallahehab3710@gmail.com>
2026-04-18 14:03:05 +0200

    Refactor migrations to use references for user, post, and editor associations

--- db/migrate/20260418115307_create_post_editors.rb
+++ db/migrate/20260418115307_create_post_editors.rb
@@ -1,8 +1,8 @@
 class CreatePostEditors < ActiveRecord::Migration[8.1]
   def change
     create_table :post_editors do |t|
-      t.integer :post_id
-      t.integer :editor_id
+      t.references :post, null: false, foreign_key: true
+      t.references :editor, null: false, foreign_key: true
 
       t.timestamps
     end
